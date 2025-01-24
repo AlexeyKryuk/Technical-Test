9d6c2032804c57806c73f7ebdaa4c8c5f2af8c9e
Lightel <43128289+AlexeyKryuk@users.noreply.github.com>
2025-01-24 14:27:56 +0300

    Created CompositeRoot

---
+++ Assets/Source/Code/CompositeRoot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CompositeRoot : MonoBehaviour
+{
+    [SerializeField] private LoadingScreenView _loadingScreenView;
+    [SerializeField] private CounterScreenView _counterScreenView;
+
+    private AssetProvider _assetProvider;
+    private GeneralDataLoader _dataLoader;
+
+    private LoadingDataViewModel _loadingDataViewModel;
+    private UpdateDataViewModel _updateDataViewModel;
+
+    private void Awake()
+    {
+        Compose();
+    }
+
+    public async void Compose()
+    {
+        _assetProvider = new AssetProvider();
+
+        var localLoader = new LocalDataLoader(_assetProvider, _assetProvider.LocalDataPath);
+        var remoteLoader = new RemoteDataLoader(_assetProvider, _assetProvider.Url);
+
+        _dataLoader = new GeneralDataLoader(localLoader, remoteLoader);
+        _loadingDataViewModel = new LoadingDataViewModel(_loadingScreenView, _dataLoader);
+
+        var loadTask = _loadingDataViewModel.LoadData();
+
+        await loadTask;
+
+        _updateDataViewModel = new UpdateDataViewModel(_counterScreenView, _assetProvider, _dataLoader, localLoader);
+        _updateDataViewModel.Initialize();
+    }
+
+    private void OnDisable()
+    {
+        _updateDataViewModel.Dispose();
+    }
+}
