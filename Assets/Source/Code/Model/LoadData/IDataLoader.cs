fff6255f314a6bc3ed86ccd2c5eb449016d271e5
Lightel <43128289+AlexeyKryuk@users.noreply.github.com>
2025-01-24 14:22:04 +0300

    Create infrusctructure

---
+++ Assets/Source/Code/Model/LoadData/IDataLoader.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+public interface IDataLoader
+{
+    Task<string> LoadJsonData(string fileName);
+    Task<T> LoadAsset<T>(string key) where T : Object;
+}
